a4c83f694f340b01a754eecaa45163806dd00b8f
chryschi <121822197+chryschi@users.noreply.github.com>
2024-05-23 13:29:47 +0200

    Pick desired data from fetched data

--- src/index.js
+++ src/index.js
@@ -7,35 +7,76 @@ location = "london";
 
 const forecastdays = 3;
 
-const getCurrentWeather = async (location) => {
-  const response = await fetch(
-    `https://api.weatherapi.com/v1/current.json?key=6e4aa9685cba4408b05220807242205&q=${location}`
-  );
-  const currentWeatherData = await response.json();
-  console.log("Location:");
-  console.log(
-    `${currentWeatherData.location.name} (${currentWeatherData.location.country})`
-  );
-  console.log("Temperature (°C):");
-  console.log(currentWeatherData.current.temp_c);
+const createCleanWeatherData = (weatherObject) => {
+  const location = weatherObject.location.name;
+  const region = weatherObject.location.region;
+  const country = weatherObject.location.country;
+  const currentTempCelsius = weatherObject.current.temp_c;
+  const currentTempFahrenheit = weatherObject.current.temp_f;
+  const feelsLikeCelsius = weatherObject.current.feelslike_c;
+  const feelsLikeFahrenheit = weatherObject.current.feelslike_f;
+  const humidity = weatherObject.current.humidity;
+
+  let forecastdayArray = [];
+  for (let i = 0; i < forecastdays; i++) {
+    const forecastDay = weatherObject.forecast.forecastday[i];
+    const date = forecastDay.date;
+    const minTempCelsius = forecastDay.day.mintemp_c;
+    const maxTempCelsius = forecastDay.day.maxtemp_c;
+    const minTempFahrenheit = forecastDay.day.mintemp_f;
+    const maxTempFahrenheit = forecastDay.day.maxtemp_f;
+    const conditionCode = forecastDay.day.condition.code;
+    const conditionText = forecastDay.day.condition.text;
+    const conditionIcon = forecastDay.day.condition.icon;
+    const forecastDayObject = {
+      date,
+      minTempCelsius,
+      maxTempCelsius,
+      minTempFahrenheit,
+      maxTempFahrenheit,
+      conditionCode,
+      conditionText,
+      conditionIcon,
+    };
+    forecastdayArray.push(forecastDayObject);
+  }
+
+  return {
+    location,
+    region,
+    country,
+    currentTempCelsius,
+    currentTempFahrenheit,
+    feelsLikeCelsius,
+    feelsLikeFahrenheit,
+    humidity,
+    forecastdayArray,
+  };
 };
 
-const getForecast = async (location) => {
+const getWeatherData = async (location) => {
   const response = await fetch(
     `https://api.weatherapi.com/v1/forecast.json?key=6e4aa9685cba4408b05220807242205&q=${location}&days=${forecastdays}`
   );
-  const forecastData = await response.json();
+  const weatherData = await response.json();
   console.log("Location:");
-  console.log(
-    `${forecastData.location.name} (${forecastData.location.country})`
-  );
+  console.log(`${weatherData.location.name} (${weatherData.location.country})`);
+  console.log("Temperature (°C):");
+  console.log(weatherData.current.temp_c);
+  console.log("Location:");
+  console.log(`${weatherData.location.name} (${weatherData.location.country})`);
   console.log("Avg. Temperature today (°C):");
-  console.log(forecastData.forecast.forecastday[0].day.avgtemp_c);
+  console.log(weatherData.forecast.forecastday[0].day.avgtemp_c);
   console.log("Avg. Temperature tomorrow (°C):");
-  console.log(forecastData.forecast.forecastday[1].day.avgtemp_c);
+  console.log(weatherData.forecast.forecastday[1].day.avgtemp_c);
   console.log("Avg. Temperature day after tomorrow (°C):");
-  console.log(forecastData.forecast.forecastday[2].day.avgtemp_c);
+  console.log(weatherData.forecast.forecastday[2].day.avgtemp_c);
+  console.log(weatherData);
+  console.log(weatherData.forecast.forecastday[0].date);
+
+  const cleanWeatherData = createCleanWeatherData(weatherData);
+  console.log("Cleaned up data:");
+  console.log(cleanWeatherData);
 };
 
-getCurrentWeather(location);
-getForecast(location);
+getWeatherData(location);
